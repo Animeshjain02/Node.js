fb235a5ca5a31191e7eda03b65ca384cc351998d
Animeshjain02 <156539978+Animeshjain02@users.noreply.github.com>
2025-04-06 18:28:38 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './src/**/*.{html,js}',  // Specify the paths to your HTML/JS files here
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
